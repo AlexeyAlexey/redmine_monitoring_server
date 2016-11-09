f24d5ee633e11c2325cdc20f387f74459159e678
Alexey Kondratenko <a.kondratenko@smileexpo.ru>
2016-11-09 15:32:51 +0200

    added charts

--- app/controllers/monitoring_results_controller.rb
+++ app/controllers/monitoring_results_controller.rb
@@ -1,5 +1,7 @@
 class MonitoringResultsController < ApplicationController
   unloadable
+  helper MonitoringResultsHelper
+  
   before_filter :require_login
   #before_filter :find_project_by_project_id
   before_filter :find_project
@@ -7,7 +9,11 @@ class MonitoringResultsController < ApplicationController
 
 
   def index
-  	@monitoring_result = @project.monitoring_results.first
+  	@monitoring = @project.monitoring_results.first
+    
+    @controllers_list = @monitoring.result.keys
+    @controllers_list.delete_at(0)
+    @controllers_list.delete_at(0)
 
   end
 
